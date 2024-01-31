a8bc1fb796a42728cfa53b99a804d94f09f57894
Exequiel <Exequiel>
2024-01-31 17:14:46 -0300

    mostrar algo de info en el dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Alumno;
+use App\Models\Curso;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $user = Auth::user();
+
+        // Inicializar la variable para almacenar la cantidad de cursos
+        $cantidadCursos = 0;
+        $cantidadAlumnos = 0;
+
+        // Verifica si el usuario tiene el rol de "super-admin" o "admin"
+        if ($user->hasRole(['Super Admin', 'Admin'])) {
+            // Si tiene el rol de "Super Admin" o "Admin", obtén la cantidad de todos los cursos
+            $cantidadCursos = Curso::count();
+            $cantidadAlumnos = Alumno::count();
+        } elseif ($user->hasRole('Docente')) {
+            // Verifica si el usuario tiene el rol de "docente"
+            // Obtén la cantidad de cursos del usuario
+            $cantidadCursos = $user->cursos()->count();
+            $cantidadAlumnos = $user->alumnos()->count();
+        }
+
+        return view('dashboard.index', compact('cantidadCursos', 'cantidadAlumnos'));
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -11,17 +11,16 @@
                         <div class="card-header p-3 pt-2">
                             <div
                                 class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
-                                <i class="material-icons opacity-10">weekend</i>
+                                <i class="material-icons opacity-10">history_edu</i>
                             </div>
                             <div class="text-end pt-1">
-                                <p class="text-sm mb-0 text-capitalize">Today's Money</p>
-                                <h4 class="mb-0">$53k</h4>
+                                <p class="text-sm mb-0 text-capitalize">Cursos</p>
+                                <h4 class="mb-0">{{ $cantidadCursos }}</h4>
                             </div>
                         </div>
                         <hr class="dark horizontal my-0">
                         <div class="card-footer p-3">
-                            <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than
-                                lask week</p>
+                            <p class="mb-0"><span class="text-success text-sm font-weight-bolder"></span></p>
                         </div>
                     </div>
                 </div>
@@ -33,14 +32,13 @@
                                 <i class="material-icons opacity-10">person</i>
                             </div>
                             <div class="text-end pt-1">
-                                <p class="text-sm mb-0 text-capitalize">Today's Users</p>
-                                <h4 class="mb-0">2,300</h4>
+                                <p class="text-sm mb-0 text-capitalize">Alumnos</p>
+                                <h4 class="mb-0">{{ $cantidadAlumnos }}</h4>
                             </div>
                         </div>
                         <hr class="dark horizontal my-0">
                         <div class="card-footer p-3">
-                            <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+3% </span>than
-                                lask month</p>
+                            <p class="mb-0"><span class="text-success text-sm font-weight-bolder"></span></p>
                         </div>
                     </div>
                 </div>
@@ -49,17 +47,16 @@
                         <div class="card-header p-3 pt-2">
                             <div
                                 class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
-                                <i class="material-icons opacity-10">person</i>
+                                <i class="material-icons opacity-10">calendar_month</i>
                             </div>
                             <div class="text-end pt-1">
                                 <p class="text-sm mb-0 text-capitalize">New Clients</p>
-                                <h4 class="mb-0">3,462</h4>
+                                <h4 class="mb-0">{{ $cantidadAlumnos }}</h4>
                             </div>
                         </div>
                         <hr class="dark horizontal my-0">
                         <div class="card-footer p-3">
-                            <p class="mb-0"><span class="text-danger text-sm font-weight-bolder">-2%</span> than
-                                yesterday</p>
+                            <p class="mb-0"><span class="text-danger text-sm font-weight-bolder"></span></p>
                         </div>
                     </div>
                 </div>
@@ -77,8 +74,7 @@
                         </div>
                         <hr class="dark horizontal my-0">
                         <div class="card-footer p-3">
-                            <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+5% </span>than
-                                yesterday</p>
+                            <p class="mb-0"><span class="text-success text-sm font-weight-bolder"></span></p>
                         </div>
                     </div>
                 </div>
